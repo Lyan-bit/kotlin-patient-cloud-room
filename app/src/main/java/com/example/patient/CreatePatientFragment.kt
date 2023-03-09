cd05e57dc6ffe4da41e2527e733742d799292285
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-03-09 22:46:20 +0000

    Update CreatePatientFragment.kt

--- app/src/main/java/com/example/patient/CreatePatientFragment.kt
+++ app/src/main/java/com/example/patient/CreatePatientFragment.kt
@@ -77,15 +77,15 @@ class CreatePatientFragment : Fragment(), View.OnClickListener {
 		}
 		when (v?.id) {
 			R.id.crudPatientOK-> {
-				crudPatientOK()
+				createPatientOK()
 			}
 			R.id.crudPatientCancel-> {
-				crudPatientCancel()
+				deletePatientCancel()
 			}
 		}
 	}
 
-	private fun crudPatientOK () {
+	private fun deletePatientOK () {
 		patientIdData = patientIdTextField.text.toString()
 		patientBean.setPatientId(patientIdData)
 		nameData = nameTextField.text.toString()
@@ -105,7 +105,7 @@ class CreatePatientFragment : Fragment(), View.OnClickListener {
 			}
 	}
 
-	private fun crudPatientCancel () {
+	private fun createPatientCancel () {
 		patientBean.resetData()
 		patientIdTextField.setText("")
 		nameTextField.setText("")
