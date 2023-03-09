db982ade551034afbe3f5ee72f3c37cdeed1e8aa
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-03-09 21:19:39 +0000

    Update AppointmentBean.kt

--- app/src/main/java/com/example/patient/AppointmentBean.kt
+++ app/src/main/java/com/example/patient/AppointmentBean.kt
@@ -1,137 +1,161 @@
 package com.example.patient
-	
+
 import android.content.Context
 
-	
 class AppointmentBean(c: Context) {
-	
+
     private var model: ModelFacade = ModelFacade.getInstance(c)
-	
+
     private var appointmentId = ""
     private var code = ""
     private var patientId = ""
+
     private var errors = ArrayList<String>()
-    private var checkParameter = "is not exist"
-	
+    private var checkParameter = " is not exist"
+    private var checkEmpty = " cannot be empty"
+
     fun setAppointmentId(appointmentIdx: String) {
 	 appointmentId = appointmentIdx
     }
+    
     fun setCode(codex: String) {
 	 code = codex
     }
+    
+
     fun setPatientId(patientIdx : String) {
-		patientId = patientIdx
-	}
-	fun resetData() {
+	patientId = patientIdx
+    }
+
+    fun resetData() {
 	  appointmentId = ""
 	  code = ""
-	}
+    }
+    
     fun isCreateAppointmentError(): Boolean {
 	        
-	     errors.clear()
+	 errors.clear()
 	        
           if (appointmentId != "") {
 	//validate
 }
-	     else {
-	        errors.add("appointmentId cannot be empty")
-	     }
+	else {
+	 	  errors.add(appointmentId + checkEmpty)
+	}
           if (code != "") {
 	//validate
 }
-	     else {
-	        errors.add("code cannot be empty")
-	     }
+	else {
+	 	  errors.add(code + checkEmpty)
+	}
 
-	     return errors.size > 0
-	 }
-	
-	    suspend fun createAppointment() {
-	        model.createAppointment(AppointmentEntity(appointmentId, code))
+	        return errors.size > 0
+	    }
+	    
+	    fun createAppointment() {
+	        model.createAppointment(AppointmentVO(appointmentId, code))
 	        resetData()
 	    }
-	
-	    fun isListAppointmentError(): Boolean {
-	 	  errors.clear()
-		  //if statement
-	      return errors.size > 0
-	 }
-	 	    
 
-	fun isEditAppointmentError(allAppointmentappointmentIds: List<String>): Boolean {
+    fun isListAppointmentError(): Boolean {
 	        errors.clear()
-				
+		//if statement
+	        return errors.size > 0
+	    }
+
+     fun editAppointment() {
+		     model.editAppointment(AppointmentVO(appointmentId, code))
+		     resetData()
+		 }
+		       
+		 fun isEditAppointmentError(allAppointmentappointmentIds: List<String>): Boolean {
+	        
+	        errors.clear()
+			
 			if (!allAppointmentappointmentIds.contains(appointmentId)) {
-				errors.add("appointmentId" + checkParameter)
-			 }
-		        
-          if (appointmentId != "") {
-	//validate
-}
-	        else {
-	            errors.add("appointmentId cannot be empty")
+				errors.add(appointmentId + checkParameter)
+		    }
+			          if (appointmentId != "") {
+	        	//validate
 	        }
-	          if (code != "") {
-	//validate
-}
-	        else {
-	            errors.add("code cannot be empty")
+	         else {
+	               errors.add(appointmentId + checkEmpty)
+	         }
+	                  if (code != "") {
+	        	//validate
 	        }
-	
-	        return errors.size > 0
+	         else {
+	               errors.add(code + checkEmpty)
+	         }
+	        
+       return errors.size > 0
+   }
+
+	    fun deleteAppointment() {
+	        model.deleteAppointment(appointmentId)
+	        resetData()
 	    }
-
-    suspend fun editAppointment() {
-	      model.editAppointment(AppointmentEntity(appointmentId, code))
-	      resetData()
-	 }
-
+	    
 	    fun isDeleteAppointmentError(allAppointmentappointmentIds: List<String>): Boolean {
-		 errors.clear()
-	     if (!allAppointmentappointmentIds.contains(appointmentId)) {
-		   errors.add("appointmentId" + checkParameter)
- 	     }
-	     return errors.size > 0
-	  }    
-		
-     suspend fun deleteAppointment() {
-	       model.deleteAppointment(appointmentId)
-          resetData()
-	  }
-	    
-	
-			fun isSearchAppointmentIdError(allAppointmentIds: List<String>): Boolean {
-    	    errors.clear()
-    	    if (!allAppointmentIds.contains(appointmentId)) {
-    	        errors.add("appointmentId" + checkParameter)
-    	        }
-    	    return errors.size > 0
-     }
-	
-	    fun errors(): String {
-	        return errors.toString()
-	    }
-	
-	    fun isAddPatientattendsAppointmentError(): Boolean {
-	        errors.clear()
-	        return errors.size > 0
-	    }
-	    
-	    fun addPatientattendsAppointment() {
-	         model.addPatientattendsAppointment(appointmentId, patientId)
-	         resetData()
-	    }
-	    
-    	    fun isRemovePatientattendsAppointmentError(): Boolean {
-	        errors.clear()
-	        return errors.size > 0
-	    }
-	    
-	    fun removePatientattendsAppointment() {
-	         model.removePatientattendsAppointment(appointmentId, patientId)
-	         resetData()
-	    }
-	    
-	
-	
-	}
+	         errors.clear()
+			 if (!allAppointmentappointmentIds.contains(appointmentId)) {
+			    errors.add(appointmentId + checkParameter)
+	         }
+	         return errors.size > 0
+		}    
+
+
+		fun isSearchAppointmentIdError(allAppointmentIds: List<String>): Boolean {
+    	   errors.clear()
+   	       if (!allAppointmentIds.contains(appointmentId)) {
+    	       errors.add(appointmentId + checkParameter)
+    	   }
+           return errors.size > 0
+    }
+
+    fun errors(): String {
+        return errors.toString()
+    }
+
+   fun isAddPatientattendsAppointmentError(): Boolean {
+        errors.clear()
+	if (appointmentId != "" ) {
+		   //ok
+	   } else
+		   errors.add(appointmentId + checkEmpty)
+
+	if ( patientId != "") {
+		   //ok
+	   } else
+		   errors.add(patientId + checkEmpty)
+        return errors.size > 0
+    }
+
+    fun addPatientattendsAppointment() {
+         model.addPatientattendsAppointment(appointmentId, patientId)
+         resetData()
+    }
+    
+   fun isRemovePatientattendsAppointmentError(): Boolean {
+        errors.clear()
+	if (appointmentId != "" ) {
+		   //ok
+	   } else
+		   errors.add(appointmentId + checkEmpty)
+
+	if ( patientId != "" && patientId != "Null") {
+		   //ok
+	   } else
+		   errors.add(patientId + checkEmpty)
+        return errors.size > 0
+    }
+
+    fun removePatientattendsAppointment() {
+         model.removePatientattendsAppointment(appointmentId, patientId)
+         resetData()
+    }
+    
+
+
+}
+
